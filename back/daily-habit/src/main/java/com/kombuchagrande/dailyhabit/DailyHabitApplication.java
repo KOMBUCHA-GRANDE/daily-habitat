f66d5278302182b507dc2a1d1c5f917d5da4f130
reflash407 <reflash407@naver.com>
2025-04-23 00:49:26 +0900

    feat: 백엔드 프로젝트 초기 세팅

---
+++ back/daily-habit/src/main/java/com/kombuchagrande/dailyhabit/DailyHabitApplication.java
@@ -0,0 +1,13 @@
+package com.kombuchagrande.dailyhabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHabitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyHabitApplication.class, args);
+    }
+
+}
