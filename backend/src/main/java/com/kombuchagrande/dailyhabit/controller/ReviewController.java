bed255178a18e467ab1b0dec5047260ad127dbd6
yxoni <psy337250@gmail.com>
2025-08-14 17:15:25 +0900

    feat: ReviewController.get 작성

--- backend/src/main/java/com/kombuchagrande/dailyhabit/controller/ReviewController.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/controller/ReviewController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,15 @@ public class ReviewController {
 
     return ResponseEntity
         .status(HttpStatus.CREATED)
+        .body(reviewDto);
+  }
+
+  @GetMapping("/{reviewId}")
+  public ResponseEntity<ReviewDto> get(@PathVariable Long reviewId) {
+    ReviewDto reviewDto = reviewService.get(reviewId);
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
         .body(reviewDto);
   }
 }
