5bab6c988e43a6d66e4cf63da7138ca0063ffd70
yxoni <psy337250@gmail.com>
2025-08-29 17:23:54 +0900

    feat: ReviewController.getReviews 작성

--- backend/src/main/java/com/kombuchagrande/dailyhabit/controller/ReviewController.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/controller/ReviewController.java
@@ -2,9 +2,14 @@ package com.kombuchagrande.dailyhabit.controller;
 
 import com.kombuchagrande.dailyhabit.dto.review.ReviewCreateRequest;
 import com.kombuchagrande.dailyhabit.dto.review.ReviewDto;
+import com.kombuchagrande.dailyhabit.dto.review.ReviewDtoCursorResponse;
+import com.kombuchagrande.dailyhabit.entity.enums.PeriodType;
+import com.kombuchagrande.dailyhabit.enums.SortDirection;
 import com.kombuchagrande.dailyhabit.service.ReviewService;
 import jakarta.validation.Valid;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,5 +43,23 @@ public class ReviewController {
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(reviewDto);
+  }
+
+  @GetMapping
+  public ResponseEntity<ReviewDtoCursorResponse> getReviews(
+      @RequestParam Long habitId,
+      @RequestParam PeriodType periodType,
+      @RequestParam(required = false) Long lastIndex,
+      @RequestParam(defaultValue = "DESC") SortDirection sortDirection,
+      @RequestParam(required = false) String keyword,
+      @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+      @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to
+  ) {
+    ReviewDtoCursorResponse response = reviewService.getReviews(habitId, periodType, lastIndex,
+        sortDirection, keyword, from, to);
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(response);
   }
 }
