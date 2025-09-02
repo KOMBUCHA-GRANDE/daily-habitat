92443dd52f2e2ad9bb80bf0532691168fa6a4685
yxoni <psy337250@gmail.com>
2025-09-02 17:13:22 +0900

    test: ReviewRepositoryCustomImplTest에 @EnableConfigurationProperties 적용

--- backend/src/test/java/com/kombuchagrande/dailyhabit/repository/querydsl/ReviewRepositoryCustomImplTest.java
+++ backend/src/test/java/com/kombuchagrande/dailyhabit/repository/querydsl/ReviewRepositoryCustomImplTest.java
@@ -2,7 +2,6 @@ package com.kombuchagrande.dailyhabit.repository.querydsl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.kombuchagrande.dailyhabit.config.TestPagingPropertiesConfig;
 import com.kombuchagrande.dailyhabit.config.QuerydslConfig;
 import com.kombuchagrande.dailyhabit.entity.Habit;
 import com.kombuchagrande.dailyhabit.entity.Review;
@@ -19,6 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -27,8 +27,9 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 @DataJpaTest
 @EnableJpaAuditing
-@Import({QuerydslConfig.class, TestPagingPropertiesConfig.class})
+@Import(QuerydslConfig.class)
 @ActiveProfiles("test")
+@EnableConfigurationProperties(value = {})
 class ReviewRepositoryCustomImplTest {
 
   @Autowired
