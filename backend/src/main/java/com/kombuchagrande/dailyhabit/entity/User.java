269ba63a453e63b0e9fb42b5b19e82d43f6f43b3
reflash407 <reflash407@naver.com>
2025-09-04 20:51:07 +0900

    faet : User email 제거, role, providerId 추가

--- backend/src/main/java/com/kombuchagrande/dailyhabit/entity/User.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/entity/User.java
@@ -2,6 +2,7 @@ package com.kombuchagrande.dailyhabit.entity;
 
 import com.kombuchagrande.dailyhabit.entity.base.BaseSoftDeletableEntity;
 import com.kombuchagrande.dailyhabit.entity.enums.ProviderType;
+import com.kombuchagrande.dailyhabit.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,8 +14,8 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseSoftDeletableEntity {
 
-    @Column(nullable = false)
-    private String email;
+//    @Column(nullable = false)
+//    private String email;
 
     @Column(length = 50, nullable = false)
     private String nickname;
@@ -24,4 +25,12 @@ public class User extends BaseSoftDeletableEntity {
 
     @Column(nullable = false)
     private boolean notificationEnabled;
+
+
+    @Column(nullable = false, unique = true)
+    private String providerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
