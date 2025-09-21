5f904d727a9734bc86868ddaa0e5ccdd662bc18a
reflash407 <reflash407@naver.com>
2025-09-21 22:40:20 +0900

    feat : JsonAuthenticationEntryPoint 추가

---
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/security/handler/JsonAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.kombuchagrande.dailyhabit.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class JsonAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        objectMapper.writeValue(response.getOutputStream(),
+                Map.of("error", "UNAUTHORIZED", "message", "Authentication is required."));
+    }
+    //Todo 커스텀 ErrorResponse 처리
+
+}
