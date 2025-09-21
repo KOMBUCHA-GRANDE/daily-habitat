2e2076951721effd905239f80217b2a95c08333a
reflash407 <reflash407@naver.com>
2025-09-21 22:41:21 +0900

    feat : DailyHabitApplication OidcProperties 설정 추가

--- backend/src/main/java/com/kombuchagrande/dailyhabit/DailyHabitApplication.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/DailyHabitApplication.java
@@ -1,8 +1,11 @@
 package com.kombuchagrande.dailyhabit;
 
+import com.kombuchagrande.dailyhabit.oidc.provider.OidcProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(OidcProperties.class)
 @SpringBootApplication
 public class DailyHabitApplication {
 
