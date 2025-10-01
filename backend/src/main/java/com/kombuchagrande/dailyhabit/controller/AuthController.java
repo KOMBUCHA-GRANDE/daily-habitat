20e155eafed7d00420730db20ced1a466597ddb1
reflash407 <reflash407@naver.com>
2025-10-01 19:57:10 +0900

    refactor : AuthController 리펙터링

--- backend/src/main/java/com/kombuchagrande/dailyhabit/controller/AuthController.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.kombuchagrande.dailyhabit.controller;
 
 import com.kombuchagrande.dailyhabit.dto.auth.RefreshRequest;
-import com.kombuchagrande.dailyhabit.security.jwt.dto.JwtTokenResponse;
+import com.kombuchagrande.dailyhabit.security.jwt.dto.AccessTokenRenewResponse;
 import com.kombuchagrande.dailyhabit.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +18,14 @@ public class AuthController {
 
     // 액세스 토큰 재발급
     @PostMapping("/token/refresh")
-    public JwtTokenResponse refresh(@RequestBody @Valid RefreshRequest request) {
-        return authService.refresh(request.refreshToken());
+    public ResponseEntity<AccessTokenRenewResponse> refresh(
+            @RequestBody @Valid RefreshRequest request
+    ) {
+        AccessTokenRenewResponse refresh = authService.refresh(request.refreshToken());
+        return ResponseEntity.ok(refresh);
     }
 
-    // 현재 기기 로그아웃 Authorization -> 엑시스 토큰 안에 uid 사용하여 리프레시 토큰 삭제 + 엑시스 블렉리스트
+    // 현재 기기 로그아웃 Authorization -> 엑시스 토큰 안에 uid 사용하여 리프레시 토큰 삭제 + 엑시스 블렉리스트 (컷오프)
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(
             @RequestHeader(value = HttpHeaders.AUTHORIZATION) String authorization
@@ -33,7 +36,9 @@ public class AuthController {
 
     // 모든 기기 로그아웃 (컷오프 추가)
     @PostMapping("/logout/all")
-    public ResponseEntity<Void> logoutAll(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
+    public ResponseEntity<Void> logoutAll(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization
+    ) {
         authService.logoutAll(authorization);
         return ResponseEntity.noContent().build();
     }
