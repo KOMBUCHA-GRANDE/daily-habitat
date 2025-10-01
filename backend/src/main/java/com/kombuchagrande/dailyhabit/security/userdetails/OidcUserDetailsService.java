b3b518f4e97c80abf80426d4352082e3b22c7d92
reflash407 <reflash407@naver.com>
2025-10-01 20:01:57 +0900

    refactor : OidcUserDetailsService  로그인 여부 값을 추가하기 위해서 LoadOrCreateResult 중간 객체 추가

--- backend/src/main/java/com/kombuchagrande/dailyhabit/security/userdetails/OidcUserDetailsService.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/security/userdetails/OidcUserDetailsService.java
@@ -1,15 +1,18 @@
 package com.kombuchagrande.dailyhabit.security.userdetails;
 
 import com.kombuchagrande.dailyhabit.entity.User;
+import com.kombuchagrande.dailyhabit.entity.enums.Role;
 import com.kombuchagrande.dailyhabit.oidc.dto.VerifiedOidc;
 import com.kombuchagrande.dailyhabit.repository.UserRepository;
 import com.kombuchagrande.dailyhabit.security.jwt.dto.JwtPayloadDto;
+import com.kombuchagrande.dailyhabit.security.jwt.dto.LoadOrCreateResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -24,17 +27,23 @@ public class OidcUserDetailsService implements UserDetailsService {
         throw new UnsupportedOperationException("Use loadOrCreate(VerifiedOidc) for OIDC flow.");
     }
 
-    public UserDetails loadOrCreate(VerifiedOidc verifiedOidc) {
+    public LoadOrCreateResult loadOrCreate(VerifiedOidc verifiedOidc) {
         Optional<User> found = userRepository.findByProviderTypeAndProviderId(verifiedOidc.providerType(), verifiedOidc.sub());
+
+
+        boolean isNew = found.isEmpty();
         User user = found.orElseGet(() -> {
             User saveuser = User.builder()
                     .providerType(verifiedOidc.providerType())
                     .providerId(verifiedOidc.sub())
+                    .nickname("user-" + UUID.randomUUID().toString().substring(0, 8))
+                    .role(Role.USER)
+                    .notificationEnabled(false) // Todo 알림 설정도 초기 회원가입에서 허용 여부 값 받기
                     .build();
             return userRepository.save(saveuser);
         });
 
         JwtPayloadDto payload = JwtPayloadDto.fromUser(user);
-        return new CustomUserDetails(payload);
+        return new LoadOrCreateResult(new CustomUserDetails(payload), isNew);
     }
 }
