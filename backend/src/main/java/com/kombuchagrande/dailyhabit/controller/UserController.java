a38d01e60079164f9f5eea5339633236ebcef411
reflash407 <reflash407@naver.com>
2025-10-21 15:07:23 +0900

    feat : UserController api/users 엔드포인트 추가

--- backend/src/main/java/com/kombuchagrande/dailyhabit/controller/UserController.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.kombuchagrande.dailyhabit.controller;
 
+
+import com.kombuchagrande.dailyhabit.dto.user.UserDto;
+import com.kombuchagrande.dailyhabit.entity.User;
+import com.kombuchagrande.dailyhabit.mapper.UserMapper;
+import com.kombuchagrande.dailyhabit.repository.UserRepository;
+import com.kombuchagrande.dailyhabit.security.userdetails.CustomUserDetails;
+import com.kombuchagrande.dailyhabit.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(@AuthenticationPrincipal CustomUserDetails user) {
+        UserDto userDto = userService.get(user.getUserId());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userDto);
+    }
 }
