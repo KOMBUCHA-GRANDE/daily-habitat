1d49d006b6c777ed4bba479ed5d97837a1c7ddc4
reflash407 <reflash407@naver.com>
2025-10-29 11:06:24 +0900

    chore : UserController 경로에 누락된 '/' 추가

--- backend/src/main/java/com/kombuchagrande/dailyhabit/controller/UserController.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/users")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
