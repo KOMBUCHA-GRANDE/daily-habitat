77d70c84a134d8b4530173422b8871a1eb35c7e1
reflash407 <reflash407@naver.com>
2025-11-02 22:50:32 +0900

    chore : EnableConfigurationProperties 제거

--- backend/src/main/java/com/kombuchagrande/dailyhabit/DailyHabitApplication.java
+++ backend/src/main/java/com/kombuchagrande/dailyhabit/DailyHabitApplication.java
@@ -1,13 +1,9 @@
 package com.kombuchagrande.dailyhabit;
 
-import com.kombuchagrande.dailyhabit.config.PagingProperties;
-import com.kombuchagrande.dailyhabit.config.oidc.OidcProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({PagingProperties.class, OidcProperties.class})
 public class DailyHabitApplication {
 
 	public static void main(String[] args) {
